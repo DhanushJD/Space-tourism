a5ab92726ef6fc464369f8e307e6570ed4903e54
DhanushJD <100829530+DhanushJD@users.noreply.github.com>
2025-09-07 10:56:37 +0530

    Update pagesInfo.js
    resolved path issue

--- scripts/utils/pagesInfo.js
+++ scripts/utils/pagesInfo.js
@@ -2,7 +2,7 @@ export let dataDestinations;
 export let dataCrew;
 export let dataTech;
 
-const response = await fetch('../../scripts/data/data.json');
+const response = await fetch('./scripts/data/data.json');
 const data = await response.json();
 
 dataDestinations = data.destinations;
@@ -14,3 +14,4 @@ dataTech = data.technology;
 export function matchingDetails (data, matchString){
   return data.find(details => details.name === matchString || details.id === matchString);
 }
+
